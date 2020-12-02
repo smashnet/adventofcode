165f8aab3c52c0ce3f442b4f6d03588c6fbc9837
Nicolas Inden <nicolas.inden@innoq.com>
2020-12-02 20:49:30 +0100

    Added phase 2

--- 2020/day2.py
+++ 2020/day2.py
@@ -8,14 +8,20 @@ class Policy():
         occ, self.letter = policy_str.split(' ')
         self.min_occ, self.max_occ = [int(val) for val in occ.split('-')]
 
-    def isValidPassword(self, password):
+    def isValidPhase1Password(self, password):
         return self.min_occ <= list(password).count(self.letter) <= self.max_occ
+
+    def isValidPhase2Password(self, password):
+        return (password[self.min_occ-1] == self.letter) ^ (password[self.max_occ-1] == self.letter)
 
     def __str__(self):
         return f'Letter "{self.letter}", min: {self.min_occ}, max: {self.max_occ}'
 
 if __name__ == "__main__":
     with open("2020/day2_input.txt", 'r') as input:
-        res = [Policy(a.split(':')[0]).isValidPassword(a.split(':')[1].strip()) for a in input.readlines()]
-        print("Amount of passwords that fulfill their policy:", res.count(True))
+        lines = input.readlines()
+        phase1 = [Policy(a.split(':')[0]).isValidPhase1Password(a.split(':')[1].strip()) for a in lines]
+        phase2 = [Policy(a.split(':')[0]).isValidPhase2Password(a.split(':')[1].strip()) for a in lines]
+        print("Phase 1: Amount of passwords that fulfill their policy:", phase1.count(True))
+        print("Phase 2: Amount of passwords that fulfill their policy:", phase2.count(True))
             
